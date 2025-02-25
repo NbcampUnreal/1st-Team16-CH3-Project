d8033e5c78c02fc3a9b43371d095e3f951f7e4cf
gadada <aug120jw@gmail.com>
2025-02-25 13:36:44 +0900

    Set_Project

---
+++ Source/Timed_Survival/Timed_Survival.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Timed_Survival.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Timed_Survival, "Timed_Survival" );
