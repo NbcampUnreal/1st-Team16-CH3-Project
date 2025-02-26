225444c936297f46204e531d2f5384f7db5ba6bc
Choisehun3695 <choisehun3695@gmail.com>
2025-02-26 12:23:03 +0900

    weapon update

--- Source/Timed_Survival/Weapon/Axe.cpp
+++ Source/Timed_Survival/Weapon/Axe.cpp
@@ -1,8 +1,37 @@
 
 #include "Axe.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
 AAxe::AAxe()
 {
+	AttackRange = 100.0f;
 	Damage = 10;
 }
 
+void AAxe::Attack()
+{
+	Super::Attack();
+
+	OnHit();
+}
+
+void AAxe::OnHit()
+{
+    FVector Start = GetOwner()->GetActorLocation();
+    FVector End = Start + GetOwner()->GetActorForwardVector() * AttackRange;
+
+    FHitResult HitResult;
+    FCollisionQueryParams CollisionParams;
+    CollisionParams.AddIgnoredActor(this);
+    CollisionParams.AddIgnoredActor(GetOwner());
+
+    if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Pawn, CollisionParams))
+    {
+        if (AActor* HitActor = HitResult.GetActor())
+        {
+            UGameplayStatics::ApplyDamage(HitActor, Damage, GetOwner()->GetInstigatorController(), this, DamageType);
+        }
+    }
+}
