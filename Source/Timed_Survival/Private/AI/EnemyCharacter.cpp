b5e4313f2078257640073f2609956a1188621183
JKs-ui144 <yjlo3521@gmail.com>
2025-02-26 17:03:12 +0900

    fix_BuildError
    EnemyCharacter.cpp_fix_Error

--- Source/Timed_Survival/Private/AI/EnemyCharacter.cpp
+++ Source/Timed_Survival/Private/AI/EnemyCharacter.cpp
@@ -17,18 +17,18 @@ AEnemyCharacter::AEnemyCharacter()
 	Damage = 10.f;
 }
 
-void AEnemyCharacter::TakeDamage(int Damage)
-{
-	if (CurrentHP > 0)
-	{
-		CurrentHP = FMath::Clamp(CurrentHP - Damage, 0, MaxHP);
-	}
-}
-
-void AEnemyCharacter::TakeHeadShot(int Damage)
-{
-	// 데미지값을 어디서 할지 고민좀 해보겠습니다..
-}
+//void AEnemyCharacter::TakeDamage(int Damage)
+//{
+//	if (CurrentHP > 0)
+//	{
+//		CurrentHP = FMath::Clamp(CurrentHP - Damage, 0, MaxHP);
+//	}
+//}
+//
+//void AEnemyCharacter::TakeHeadShot(int Damage)
+//{
+//	// 데미지값을 어디서 할지 고민좀 해보겠습니다..
+//}
 
 void AEnemyCharacter::AIOnDeath()
 {
