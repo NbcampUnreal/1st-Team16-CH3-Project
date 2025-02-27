a66af66a68dc8fb2136299d079315571b274997d
JKs-ui144 <yjlo3521@gmail.com>
2025-02-27 17:15:43 +0900

    fix_name

--- Source/Timed_Survival/Public/AI/BTService_TSSightPlayer.h
+++ Source/Timed_Survival/Public/AI/BTService_TSSightPlayer.h
@@ -5,13 +5,6 @@
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTService.h"
 #include "BTService_TSSightPlayer.generated.h"
-
-//UENUM(BlueprintType)
-//enum class ETS_AISense : uint8
-//{
-//	E_None UMETA(DisplayName = "None"),
-//	E_Sight UMETA(DisplayName = "Sight")
-//};
 /**
  * 
  */
@@ -21,15 +14,5 @@ class TIMED_SURVIVAL_API UBTService_TSSightPlayer : public UBTService
 	GENERATED_BODY()
 	
 public:
-	UBTService_TSSightPlayer(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
-protected:
-	//virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds);
-
-
-	//UFUNCTION()
-	//FAIStimulus CanSenseActor(AActor* Actor, EAISense Sense);
-public:
-	//UFUNCTION()
-	//void PerceptionUpdated(const TArray<AActor*>& UpdatedActors);
 };
