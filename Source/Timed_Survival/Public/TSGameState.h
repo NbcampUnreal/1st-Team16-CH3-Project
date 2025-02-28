acde2c66e15a33f813ef7b88eda1a261ddf5764b
gadada <aug120jw@gmail.com>
2025-02-28 13:14:37 +0900

    add.funtion

--- Source/Timed_Survival/Public/TSGameState.h
+++ Source/Timed_Survival/Public/TSGameState.h
@@ -58,5 +58,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Count")
 	void IncreaseHealingCount(int32 Amount);
 
+	// Count Bullet Items
+	void FindARBullet();
+	void FindPistolBullet();
+
 	void UpdateHealth();
 };
